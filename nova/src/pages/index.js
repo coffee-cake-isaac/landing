019dbc4885cc044b17a98b98d153f013b1b1d481
Isaac <isaac.barham.developer@gmail.com>
2024-05-03 14:42:52 -0400

    Reorg

--- nova/src/pages/index.js
+++ nova/src/pages/index.js
@@ -1,88 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
-
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
-
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
-
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const IndexPage = () => (
   <>
     <div className="max-w-6xl mx-auto">
-      <div className="max-w-6xl">
+      <div className="max-w-6xl mt-20">
         <div className="flex justify-center"></div>
-        <div className="flex justify-center">
-          <StaticImage
-            src="../images/logo.png"
-            placeholder="blurred"
-            layout="fixed"
-            width={200}
-            alt="Our logo"
-          />
-        </div>
+        <div className="flex justify-center"></div>
         <div className="text-5xl md:text-8xl font-bold text-center w-full">
           Your website is your handshake to your
         </div>
@@ -98,9 +24,9 @@ const IndexPage = () => (
             customers.
           </p>
           <div className="flex text-center justify-center">
-            <ul className="flex flex-col justify-around">
-              <li>
-                <div class="max-w-sm rounded-xl overflow-hidden shadow-2xl">
+            <ul className="flex flex-col justify-around m-2">
+              <li className="">
+                <div class="max-w-sm rounded-xl overflow-hidden shadow-2xl bg-slate-100">
                   <div class="p-4">
                     <div class="font-bold text-xl mb-2 underline">
                       Erin Emily Photography
@@ -113,7 +39,7 @@ const IndexPage = () => (
                 </div>
               </li>
               <li>
-                <div class="max-w-sm rounded-xl overflow-hidden shadow-2xl shadow-indigo-500/40">
+                <div class="max-w-sm rounded-xl overflow-hidden shadow-2xl bg-slate-100">
                   <div class="p-4">
                     <div class="font-bold text-xl mb-2 underline">
                       Blue Otter Studios
@@ -146,7 +72,7 @@ const IndexPage = () => (
         </div>
       </div>
       <div>
-        <h1 className="md:text-5xl text-center font-light">
+        <h1 className="text-3xl md:text-5xl text-center font-light">
           Your website is often the first impression that your guests and
           community will have of you. It's important to exude the same{" "}
           <span className="text-transparent font-extrabold bg-clip-text bg-gradient-to-br from-amber-400 via-amber-400 to-red-400">
@@ -159,23 +85,75 @@ const IndexPage = () => (
           that you would in person.
         </h1>
         <div className="flex grid grid-cols-1 md:grid-cols-2 items-center justify-center">
-          <div className="block m-5 p-6 bg-white rounded-lg bg-gray-100 dark:border-gray-500 ">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-              Noteworthy technology acquisitions 2022
+          <div className="block m-5 p-6 bg-white rounded-lg bg-slate-100 dark:border-gray-500 ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
+              Why Choose Blue Otter Studios?
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
+            <div className="my-5">
+              <div className="inline font-semibold text-xl">
+                <p>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="text-green-400 inline"
+                  >
+                    <polyline points="20 6 9 17 4 12"></polyline>
+                  </svg>{" "}
+                  Relentless of Pursuit of Perfection
+                </p>
+              </div>
+              <div className="inline font-semibold text-xl">
+                <p>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="text-green-400 inline"
+                  >
+                    <polyline points="20 6 9 17 4 12"></polyline>
+                  </svg>{" "}
+                  Unique websites via hand-crafted websites
+                </p>
+              </div>
+              <div className="inline font-semibold text-xl">
+                <p>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="text-green-400 inline"
+                  >
+                    <polyline points="20 6 9 17 4 12"></polyline>
+                  </svg>{" "}
+                  Seamless Collaboration
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="block m-5 p-6 bg-white rounded-lg bg-gray-100 dark:border-gray-500 ">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
+          <div className="flex justify-center items-center">
+            <StaticImage
+              src="../images/logo.png"
+              placeholder="blurred"
+              layout="fixed"
+              width={500}
+              alt="Our logo"
+            />
           </div>
         </div>
       </div>
@@ -188,11 +166,6 @@ const IndexPage = () => (
   </>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
