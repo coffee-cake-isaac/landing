a503aa8992c8d723f37a7372d75c25027e4f34de
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2024-05-06 10:41:02 -0400

    Fixed form?

--- nova/src/pages/index.js
+++ nova/src/pages/index.js
@@ -209,7 +209,7 @@ const IndexPage = () => (
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
           <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in improving your web presence and propelling your business forward?</p>
-          <form name="contact" class="space-y-8" netlify>
+          <form name="contact" class="space-y-8" data-netlify="true">
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
               <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="isaac@blueotterstudios.com" required />
