3c9a77187cc8639c80b06d099d0266aeaa019746
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2024-05-06 11:10:17 -0400

    Gatsby help?

--- nova/src/pages/index.js
+++ nova/src/pages/index.js
@@ -210,6 +210,7 @@ const IndexPage = () => (
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in improving your web presence and propelling your business forward?</p>
           <form name="contact" method="POST" className="space-y-8" netlify>
+            <input type="hidden" name="form-name" value="contact" />
             <div>
               <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
               <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="isaac@blueotterstudios.com" required />
