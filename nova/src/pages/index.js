11e6a7ad36c2989f6ed76d21ede190eb9c414504
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2024-05-06 10:44:21 -0400

    Fixed form?

--- nova/src/pages/index.js
+++ nova/src/pages/index.js
@@ -200,29 +200,29 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-      <div class="relative flex py-5 items-center">
-        <div class="flex-grow border-t border-gray-400"></div>
-        <span class="flex-shrink mx-4 text-gray-400 text-2xl font-semibold">Let's get to work</span>
-        <div class="flex-grow border-t border-gray-400"></div>
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 text-gray-400 text-2xl font-semibold">Let's get to work</span>
+        <div className="flex-grow border-t border-gray-400"></div>
       </div>
-      <section class="bg-white">
-        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
-          <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in improving your web presence and propelling your business forward?</p>
-          <form name="contact" class="space-y-8" data-netlify="true">
+      <section className="bg-white">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in improving your web presence and propelling your business forward?</p>
+          <form name="contact" className="space-y-8" data-netlify="true">
             <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="isaac@blueotterstudios.com" required />
+              <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+              <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="isaac@blueotterstudios.com" required />
             </div>
             <div>
-              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required />
+              <label for="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
+              <input type="text" name="subject" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required />
             </div>
-            <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+            <div className="sm:col-span-2">
+              <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <textarea id="message" name="content" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
             </div>
-            <button type="submit" class="py-3 px-5 text-sm font-medium text-center bg-blue-500 text-white max-w-full w-full min-w-full rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
+            <button type="submit" className="py-3 px-5 text-sm font-medium text-center bg-blue-500 text-white max-w-full w-full min-w-full rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
           </form>
         </div>
       </section>
